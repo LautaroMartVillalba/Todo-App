dd09d8e81022f536c8e5c9b55f3a0f1b1d1a2f65
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-06-23 20:29:53 -0300

    ADD: create_task() now can add multiple tasks without break json format (only one task for execution)

--- task_manager.py
+++ task_manager.py
@@ -1,25 +1,45 @@
 import task
 import os
 
-default_directory = './app_files'
+default_directory = './app_files/tasks.json'
 open_brace = '{'
 closed_brace = '}'
 
 def create_task(title, description, init_date, termination_date, images_directories, files_directories):
     new_task = task.Task(title, description, init_date, termination_date, images_directories, files_directories)
 
-    with open(default_directory+'/pito.json', 'a') as file:
-        file.write(
-f"""{open_brace}
-"title":"{title}",
-"description":"{description}",
-"init_date":"{init_date}",
-"termination_date":"{termination_date}",
-"images_directories":"{images_directories}",
-"files_directories":"{files_directories}"
-{closed_brace}""")
+    if not os.path.isfile(default_directory):
+        create_json_at_frist_time = open(default_directory, 'w')
+        create_json_at_frist_time.write(
+"""{
+}""")
+        create_json_at_frist_time.close()
 
-        file.close()
+    task_info =\
+f""""task1": {open_brace}
+  "title":"{new_task.title}",
+  "description":"{new_task.description}",
+  "init_date":"{new_task.init_date}",
+  "termination_date":"{new_task.termination_date}",
+  "images_directories":"{new_task.images_directories}",
+  "files_directories":"{new_task.files_directories}"
+{closed_brace}
+"""
+
+    created_json = open(default_directory, 'r')
+    created_json_list = created_json.readlines()
+    file = open(default_directory, 'w')
+
+    if created_json_list.__len__() > 9:
+            created_json_list.insert(-1, ',')
+
+    created_json_list.insert(-1, task_info)
+    print(created_json_list)
+    for line in created_json_list:
+        file.write(line)
+
+    file.close()
+    created_json.close()
 
     return new_task
 
