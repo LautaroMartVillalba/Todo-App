69c2601ca0f570d602cfd8e9c233ae1513fd3f89
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-06-24 23:32:14 -0300

    REFACTOR: change the custom implementation to json component features

--- task_manager.py
+++ task_manager.py
@@ -1,9 +1,8 @@
 import task
 import os
+import json
 
 default_directory = './app_files/tasks.json'
-open_brace = '{'
-closed_brace = '}'
 
 def create_task(title, description, init_date, termination_date, images_directories, files_directories):
     new_task = task.Task(title, description, init_date, termination_date, images_directories, files_directories)
@@ -13,37 +12,32 @@ def create_task(title, description, init_date, termination_date, images_director
     return new_task
 
 def add_task_to_json(task):
+    # If the file does not exist, it creates.
     if not os.path.isfile(default_directory):
         create_json_at_frist_time = open(default_directory, 'w')
-        create_json_at_frist_time.write(
-"""{
-}""")
+        json.dump({}, create_json_at_frist_time, indent=2)
         create_json_at_frist_time.close()
 
-    task_in_json_format =\
-f""""task1": {open_brace}
-  "title":"{task.title}",
-  "description":"{task.description}",
-  "init_date":"{task.init_date}",
-  "termination_date":"{task.termination_date}",
-  "images_directories":"{task.images_directories}",
-  "files_directories":"{task.files_directories}"
-{closed_brace}
-"""
+    with open(default_directory, 'r') as json_read:
+        try:
+            tasks_data = json.load(json_read)
+        except json.JSONDecodeError:
+            tasks_data = {}
+            json_read.close()
+        json_read.close()
 
-    created_file_read = open(default_directory, 'r')
-    line_list_in_created_file = created_file_read.readlines()
-    created_file_write = open(default_directory, 'w')
+    task_name = len(tasks_data) +1
 
-    if line_list_in_created_file.__len__() > 9:
-        penultimate_line_in_fine = line_list_in_created_file[line_list_in_created_file.__len__()-1]
-        line_list_in_created_file.remove(penultimate_line_in_fine)
-        line_list_in_created_file.insert(-1, penultimate_line_in_fine+',\n')
+    tasks_data[task_name] = {
+            "title": task.title,
+            "description": task.description,
+            "init_date": task.init_date,
+            "termination_date": task.termination_date,
+            "images_directories": task.images_directories,
+            "files_directories": task.files_directories
+    }
 
-    line_list_in_created_file.insert(-1, task_in_json_format)
-    for line in line_list_in_created_file:
-        created_file_write.write(line)
-
-    created_file_write.close()
-    created_file_read.close()
+    with open(default_directory, 'w') as f:
+        json.dump(tasks_data, f, indent=2)
+        f.close()
 
