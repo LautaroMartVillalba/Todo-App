8a17ed66a9a927af29343ead9a8b98cbc354410c
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-07-15 22:44:44 -0300

    ADD: expose the task_manager.py methods to Api class

--- backend/index.py
+++ backend/index.py
@@ -2,6 +2,7 @@ import os
 import sys
 from time import time
 import webview
+from backend.managers import task_manager
 
 class Api:
     def fullscreen(self):
@@ -17,6 +18,57 @@ class Api:
 
     def ls(self):
         return os.listdir("")
+
+    # ///////////////////////////////////task_manager methods/////////////////////////////////////////////////
+    def create_task(self, title, description, init_date, termination_date, images_directories, files_directories):
+        try:
+            method = task_manager.create_task(title, description, init_date, termination_date, images_directories, files_directories)
+            return method
+        except Exception as e:
+            raise RuntimeError('An error has occur.') from e
+
+    def get_all_tasks(self):
+        try:
+            method = task_manager.get_all_tasks()
+            return method
+        except Exception as e:
+            raise RuntimeError('An error has occur.') from e
+
+    def get_task_by_id(self, task_id):
+        try:
+            method = task_manager.get_task_by_id(task_id)
+            return method
+        except Exception as e:
+            raise RuntimeError('An error has occur.') from e
+
+    def update_task_info_by_id(self,
+                               task_id=None,
+                               description=None,
+                               init_date=None,
+                               termination_date=None,
+                               image_id =None,
+                               new_image_directory=None,
+                               file_id =None,
+                               new_file_directory=None):
+        try:
+            method = task_manager.update_task_info_by_id(
+                                                        task_id=task_id,
+                                                        description=description,
+                                                        init_date=init_date,
+                                                        termination_date=termination_date,
+                                                        image_id=image_id,
+                                                        new_image_directory=new_image_directory,
+                                                        file_id=file_id,
+                                                        new_file_directory=new_file_directory)
+            return method
+        except Exception as e:
+            raise RuntimeError('An error has occur.') from e
+
+    def delete_task_images_and_files_by_task_id(self, task_id):
+        try:
+            task_manager.delete_task_images_and_files_by_task_id(task_id)
+        except Exception as e:
+            raise RuntimeError('An error has occur.') from e
 
 
 # This is for choosing which enviroment is the project running
