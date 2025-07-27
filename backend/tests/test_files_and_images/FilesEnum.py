db25cc9bae0fa06519b05b3de2bf7563aee2b892
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-07-27 14:06:31 -0300

    CREATE: first task_manager tests

--- backend/tests/test_files_and_images/FilesEnum.py
+++ backend/tests/test_files_and_images/FilesEnum.py
@@ -2,7 +2,7 @@ from enum import Enum
 from pathlib import Path
 
 class file(Enum):
-    THIS_FOLDER = Path().resolve().parent / 'tests/test_files_and_images'
+    THIS_FOLDER = Path().resolve().parent / 'test_files_and_images'
     CRAB_NEBULA_PDF = THIS_FOLDER / 'crab_nebula.pdf'
     HEIC_1311_A_JPG = THIS_FOLDER / 'Heic1311a.jpg'
     NGC_1514_PDF = THIS_FOLDER / 'NGC 1514.pdf'
